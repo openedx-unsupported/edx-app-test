c8e6f83c16e29516b526a33fe38c88219c2261e8
Naveed <nsaleem@edx.org>
2018-07-02 17:06:01 +0500

    LEARNER-4932 Test - iOS - Automate Main Dashboard Screen
    * Verify following contents are visible on screen, 
    ** Logged in user avatar
    ** Screen Title
    ** Account Icon
    ** Courses Tab
    ** Discovery Tab
    * Verify on tapping Courses will load Courses contents in its tab
    * Verify on tapping Discovery will load Discovery contents in its tab
    * Verify tapping user avatar will load User Profile screen
    * Verify tapping back/cancel icon from User Profile screen should get back to Main Dashboard screen
    * Verify tapping Account Icon will load Account Screen
    * Verify tapping back/cancel icon from Account Screen should get back to Main Dashboard screen
    * Above all cases are also integrated into Landscape Mode,
    * Verify following contents are visible on screen, 
    ** Logged in user avatar
    ** Screen Title
    ** Account Icon
    ** Courses Tab
    ** Discovery Tab
    * Verify that Courses tab will be selected by default
    * Verify on tapping Courses will load Courses contents in its tab
    * Verify on tapping Discovery will load Discovery contents in its tab
    * Verify tapping user avatar will load User Profile screen
    * Verify tapping back/cancel icon from User Profile screen should get back to Main Dashboard screen
    * Verify tapping Account Icon will load Account Screen
    * Verify tapping back/cancel icon from Account Screen should get back to Main Dashboard screen

--- ios/pages/ios_main_dashboard.py
+++ ios/pages/ios_main_dashboard.py
@@ -25,6 +25,58 @@ class IosMainDashboard(IosBasePage):
             ios_elements.main_dashboard_navigation_icon
         )
 
+    def get_title_textview_portrait_mode(self):
+        """
+        Get screen title
+
+        Returns:
+            webdriver elements List: Screen title textview
+        """
+
+        return self.global_contents.get_all_views_on_ios_screen(
+            self.driver,
+            ios_elements.all_otherviews
+        )[self.global_contents.twelfth_existence]
+
+    def get_profile_icon(self):
+        """
+        Get Profile Icon
+
+        Returns:
+            webdriver elements List: Profile icon
+        """
+
+        return self.global_contents.wait_and_get_element(
+            self.driver,
+            ios_elements.main_dashboard_profile_icon
+        )
+
+    def get_courses_tab(self):
+        """
+        Get Courses Tab
+
+        Returns:
+            webdriver elements List: Courses Tab
+        """
+
+        return self.global_contents.get_all_views_on_ios_screen(
+            self.driver,
+            ios_elements.all_buttons
+        )[self.global_contents.third_existence]
+
+    def get_discovery_tab(self):
+        """
+        Get Discovery Tab
+
+        Returns:
+            webdriver elements List: Discovery tab
+        """
+
+        return self.global_contents.get_all_views_on_ios_screen(
+            self.driver,
+            ios_elements.all_buttons
+        )[self.global_contents.fourth_existence]
+
     def get_account_options(self):
         """
         Click on menu drawer icon and get Account Options
@@ -40,6 +92,73 @@ class IosMainDashboard(IosBasePage):
         )
 
         return self.account_options
+
+    def load_profile_screen(self):
+        """
+        Load User Profile Screen
+
+        Returns:
+            webdriver element: Screen title textview
+        """
+
+        self.get_profile_icon().click()
+
+        return self.global_contents.get_all_views_on_ios_screen(
+            self.driver,
+            ios_elements.all_otherviews
+        )[self.global_contents.fifteenth_existence]
+
+    def load_discovery_tab(self):
+        """
+        Load Discovery
+
+        Returns:
+            webdriver elements : Discovery Tab textview
+        """
+
+        self.get_discovery_tab().click()
+
+        return self.get_discovery_tab()
+
+    def load_courses_tab(self):
+        """
+        Load Courses
+
+        Returns:
+            webdriver elements : Courses Tab textview
+        """
+
+        self.get_courses_tab().click()
+
+        return self.get_courses_tab()
+
+    def load_account_screen(self):
+        """
+        Load Account Screen
+
+        Returns:
+            webdriver element: Screen title textview
+        """
+
+        self.get_drawer_icon().click()
+
+        return self.global_contents.get_all_views_on_ios_screen(
+            self.driver,
+            ios_elements.all_otherviews
+        )[self.global_contents.fifteenth_existence]
+
+    def get_title_textview_landscape_mode(self):
+        """
+        Get screen title
+
+        Returns:
+            webdriver elements List: Screen title textview
+        """
+
+        return self.global_contents.get_all_views_on_ios_screen(
+            self.driver,
+            ios_elements.all_otherviews
+        )[self.global_contents.sixth_existence]
 
     def log_out(self):
         """
--- ios/tests/test_ios_main_dashboard.py
+++ ios/tests/test_ios_main_dashboard.py
@@ -33,26 +33,59 @@ class TestIosMainDashboard(object):
         else:
             assert ios_main_dashboard_page.get_drawer_icon().text == strings.MAIN_DASHBOARD_NAVIGATION_MENU_NAME
 
-    def test_validate_ui_elements(self, set_capabilities, setup_logging):
+    def test_validate_ui_elements_smoke(self, set_capabilities, setup_logging):
         """
         Scenarios:
                 Verify following contents are visible on screen, 
-                     Screen Title, Menu Drawer, Account Menu option and Log out user
-                Verify all screen contents have their default values
+                    Logged in user's avatar, Screen Title, Account Icon
+                    Courses Tab, Discovery Tab
+                Verify that Courses tab will be selected by default
+        """
+
+        ios_main_dashboard_page = IosMainDashboard(set_capabilities, setup_logging)
+
+        assert ios_main_dashboard_page.get_profile_icon().text == strings.MAIN_DASHBOARD_PROFILE
+        assert ios_main_dashboard_page.get_title_textview_portrait_mode().text == strings.MAIN_DASHBOARD_SCREEN_TITLE
+        assert ios_main_dashboard_page.get_drawer_icon().text == strings.MAIN_DASHBOARD_NAVIGATION_MENU_NAME
+        assert ios_main_dashboard_page.get_courses_tab().text == strings.SELECTED_BY_DEFAULT
+        assert ios_main_dashboard_page.get_discovery_tab().text == strings.MAIN_DASHBOARD_DISCOVERY_TAB
+
+    def test_load_contents_smoke(self, set_capabilities, setup_logging):
+        """
+        Scenarios:
+                Verify on tapping Courses will load Courses contents in its tab
+                Verify on tapping Discovery will load Discovery contents in its tab
+                Verify tapping user's avatar will load User Profile screen
+                Verify tapping back/cancel icon from User Profile screen should get back to Main Dashboard screen
+                Verify tapping Account Icon will load Account Screen
+                Verify tapping back/cancel icon from Account Screen should get back to Main Dashboard screen
+        """
+
+        ios_main_dashboard_page = IosMainDashboard(set_capabilities, setup_logging)
+
+        assert ios_main_dashboard_page.load_discovery_tab().text == strings.SELECTED_BY_DEFAULT
+        assert ios_main_dashboard_page.get_courses_tab().text == strings.MAIN_DASHBOARD_COURSES_TAB
+
+        assert ios_main_dashboard_page.load_courses_tab().text == strings.SELECTED_BY_DEFAULT
+        assert ios_main_dashboard_page.get_discovery_tab().text == strings.MAIN_DASHBOARD_DISCOVERY_TAB
+
+        assert ios_main_dashboard_page.load_profile_screen().text == strings.PROFILE_SCREEN_TITLE
+        set_capabilities.back()
+
+        assert ios_main_dashboard_page.load_account_screen().text == strings.ACCOUNT_SCREEN_TITLE
+        set_capabilities.back()
+
+    def test_logout_smoke(self, set_capabilities, setup_logging):
+        """
+        Scenario:
                 Verify that user can log out successfully, and back on Login screen
         """
 
         ios_main_dashboard_page = IosMainDashboard(set_capabilities, setup_logging)
         global_contents = Globals(setup_logging)
 
-        # Commenting it temporarily, it should be fix with LEARNER-4409
-        # textview_screen_title = ios_main_dashboard_page.get_title_textview()
-        # assert textview_screen_title.text == strings.MAIN_DASHBOARD_SCREEN_TITLE
-
-        assert ios_main_dashboard_page.get_drawer_icon().text == strings.MAIN_DASHBOARD_NAVIGATION_MENU_NAME
         assert ios_main_dashboard_page.get_account_options()[3].text == strings.ACCOUNT_LOGOUT
         assert ios_main_dashboard_page.log_out().text == strings.LOGIN
-
         setup_logging.info('{} is successfully logged out'.format(global_contents.login_user_name))
 
     def test_landscape_smoke(self, set_capabilities, setup_logging):
@@ -60,10 +93,16 @@ class TestIosMainDashboard(object):
         Scenarios:
                 Landscape support is added for Main Dashboard screen with following cases,
                 Change device orientation to Landscape mode
-                Verify Main Dashboard screen is loaded successfully
                 Verify following contents are visible on screen, 
-                     Screen Title, Menu Drawer, Account Menu option and Log out user
-                Verify all screen contents have their default values
+                    Logged in user's avatar, Screen Title, Account Icon
+                    Courses Tab, Discovery Tab
+                Verify that Courses tab will be selected by default
+                Verify on tapping Courses will load Courses contents in its tab
+                Verify on tapping Discovery will load Discovery contents in its tab
+                Verify tapping user's avatar will load User Profile screen
+                Verify tapping back/cancel icon from User Profile screen should get back to Main Dashboard screen
+                Verify tapping Account Icon will load Account Screen
+                Verify tapping back/cancel icon from Account Screen should get back to Main Dashboard screen
                 Verify that user can log out successfully, and back on Login screen
         """
 
@@ -75,7 +114,22 @@ class TestIosMainDashboard(object):
         setup_logging.info('{} is successfully logged in'.format(global_contents.login_user_name))
 
         global_contents.turn_orientation(set_capabilities, global_contents.LANDSCAPE_ORIENTATION)
+
+        assert ios_main_dashboard_page.get_profile_icon().text == strings.MAIN_DASHBOARD_PROFILE
+        assert ios_main_dashboard_page.get_title_textview_landscape_mode().text == strings.MAIN_DASHBOARD_SCREEN_TITLE
         assert ios_main_dashboard_page.get_drawer_icon().text == strings.MAIN_DASHBOARD_NAVIGATION_MENU_NAME
+        assert ios_main_dashboard_page.get_courses_tab().text == strings.SELECTED_BY_DEFAULT
+        assert ios_main_dashboard_page.get_discovery_tab().text == strings.MAIN_DASHBOARD_DISCOVERY_TAB
+
+        assert ios_main_dashboard_page.load_discovery_tab().text == strings.SELECTED_BY_DEFAULT
+        assert ios_main_dashboard_page.get_courses_tab().text == strings.MAIN_DASHBOARD_COURSES_TAB
+        assert ios_main_dashboard_page.load_courses_tab().text == strings.SELECTED_BY_DEFAULT
+        assert ios_main_dashboard_page.get_discovery_tab().text == strings.MAIN_DASHBOARD_DISCOVERY_TAB
+        assert ios_main_dashboard_page.load_profile_screen().text == strings.PROFILE_SCREEN_TITLE
+        set_capabilities.back()
+        assert ios_main_dashboard_page.load_account_screen().text == strings.ACCOUNT_SCREEN_TITLE
+        set_capabilities.back()
+
         assert ios_main_dashboard_page.get_account_options()[3].text == strings.ACCOUNT_LOGOUT
         assert ios_main_dashboard_page.log_out().text == strings.LOGIN
         setup_logging.info('{} is successfully logged out'.format(global_contents.login_user_name))
