a71522fe26c90a739ffbd4835e38d295dafa9b74
Naveed Saleem <naveedsaleem@naveed.local>
2021-03-03 17:46:53 +0500

    automate test execution process

---
+++ edx-trigger-execution.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function print_message(){
+  echo -e "\n************************************************\n$1\n"
+}
+
+function switch-to-project() {
+  # Switch to project part
+  cd /Users/Users/tester/Downloads/edx-app-test-master/ # replace your project path accordingly
+  print_message "switching projects"
+  source edx-setup-virtual-env.sh
+}
+
+
+function execute-android-cycle() {
+# Android Cycle
+pytest -v tests/android/tests/test_android_new_landing.py \
+tests/android/tests/test_android_whats_new.py \
+tests/android/tests/test_android_register.py \
+tests/android/tests/test_android_login.py \
+tests/android/tests/test_android_main_dashboard.py \
+tests/android/tests/test_android_account.py \
+tests/android/tests/test_android_profile.py \
+tests/android/tests/test_android_settings.py \
+tests/android/tests/test_android_my_courses_list.py \
+tests/android/tests/test_android_course_discovery.py \
+tests/android/tests/test_android_course_dashboard.py \
+tests/android/tests/test_android_course_subsection.py \
+tests/android/tests/test_android_course_html_component.py \
+--html=report.html --self-contained-html
+
+}
+
+switch-to-project
+execute-android-cycle
