f91650f8d2f83f9c890765f2bdde60fa159f3ef5
Bilal Awan <56023064+bilalawan321@users.noreply.github.com>
2021-03-04 17:19:12 +0500

    Automate Profile screen (#72)
    * Automate Profile screen

---
+++ tests/android/pages/android_profile.py
@@ -0,0 +1,209 @@
+"""
+    Profile Module
+"""
+from tests.android.pages import android_elements
+from tests.android.pages.android_base_page import AndroidBasePage
+
+
+class AndroidProfile(AndroidBasePage):
+    """
+    User Profile screen
+    """
+
+    def get_navigation_icon(self):
+        """
+        Get menu drawer icon
+
+        Returns:
+            webdriver element: menu drawer icon Element
+        """
+
+        self.global_contents.wait_for_element_visibility(
+            self.driver,
+            android_elements.course_dashboard_navigation_icon
+        )
+
+        return self.global_contents.get_all_views_on_screen(
+            self.driver,
+            android_elements.course_dashboard_navigation_icon
+        )[0]
+
+    def get_account_activity(self):
+        """
+        Load Account screen
+
+        Returns:
+            str: Account Screen Activity Name
+        """
+
+        return self.global_contents.wait_for_android_activity_to_load(
+            self.driver,
+            self.global_contents.ACCOUNT_ACTIVITY_NAME
+        )
+
+    def get_edit_profile_screen(self):
+        """
+        Get Edit Profile screen
+
+        Returns:
+            webdriver element: Edit Profile Element
+        """
+
+        return self.global_contents.wait_and_get_element(
+            self.driver,
+            android_elements.profile_screen_edit_button
+        )
+
+    def get_user_profile_image(self):
+        """
+        Load Profile screen
+
+        Returns:
+            webdriver element: User Image Element
+        """
+
+        self.global_contents.wait_for_element_visibility(
+            self.driver,
+            android_elements.profile_screen_user_image
+        )
+
+        return self.global_contents.wait_and_get_element(
+            self.driver,
+            android_elements.profile_screen_user_image
+        )
+
+    def get_user_profile_name(self):
+        """
+        Load Profile screen
+
+        Returns:
+            webdriver element: User Name Element
+        """
+
+        self.global_contents.wait_for_element_visibility(
+            self.driver,
+            android_elements.profile_screen_user_name
+        )
+
+        return self.global_contents.wait_and_get_element(
+            self.driver,
+            android_elements.profile_screen_user_name
+        )
+
+    def get_user_profile_language(self):
+        """
+        Get User Profile Language
+
+        Returns:
+            webdriver element: User Profile language Element (If language selected)
+        """
+
+        self.global_contents.wait_for_element_visibility(
+            self.driver,
+            android_elements.profile_screen_user_language
+        )
+
+        language_variable = self.global_contents.wait_and_get_element(
+            self.driver,
+            android_elements.profile_screen_user_language
+        )
+
+        if language_variable:
+            return self.global_contents.wait_and_get_element(
+                self.driver,
+                android_elements.profile_screen_user_language
+            )
+        else:
+            return None
+
+    def get_limited_profile_view(self):
+        """
+        Load Profile screen
+
+        Returns:
+            webdriver element: Limited sharing Element (If limited profile shared)
+        """
+
+        self.global_contents.wait_for_element_visibility(
+            self.driver,
+            android_elements.profile_screen_limited_view
+        )
+
+        limited_view = self.global_contents.wait_and_get_element(
+            self.driver,
+            android_elements.profile_screen_limited_view
+        )
+
+        if limited_view:
+            return self.global_contents.wait_and_get_element(
+                self.driver,
+                android_elements.profile_screen_limited_view
+            )
+        else:
+            return None
+
+    def get_user_profile_location(self):
+        """
+        Get user Profile location
+
+        Returns:
+            webdriver element: User Location Element
+        """
+
+        self.global_contents.wait_for_element_visibility(
+            self.driver,
+            android_elements.profile_screen_user_location
+        )
+
+        return self.global_contents.wait_and_get_element(
+            self.driver,
+            android_elements.profile_screen_user_location
+        )
+
+    def get_user_profile_bio(self):
+        """
+        Get User Profile bio
+
+        Returns:
+            webdriver element: User Bio Text Element
+        """
+
+        self.global_contents.wait_for_element_visibility(
+            self.driver,
+            android_elements.profile_screen_user_bio
+        )
+
+        return self.global_contents.wait_and_get_element(
+            self.driver,
+            android_elements.profile_screen_user_bio
+        )
+
+    def get_profile_age_text_note(self):
+        """
+        Get profile age text
+
+        Returns:
+              webdriver element: Profile age Element
+        """
+
+        return self.global_contents.get_all_views_on_screen(
+            self.driver,
+            android_elements.all_textviews)[self.global_contents.fifth_existence]
+
+    def get_profile_account_settings_button(self):
+        """
+        Get Account settings button in profile screen
+
+        Returns:
+            webdriver element: Account settings Element
+        """
+
+        self.global_contents.wait_for_element_visibility(
+            self.driver,
+            android_elements.profile_screen_account_settings_btn
+        )
+
+        return self.global_contents.wait_and_get_element(
+            self.driver,
+            android_elements.profile_screen_account_settings_btn
+        )
---
+++ tests/android/tests/test_android_profile.py
@@ -0,0 +1,79 @@
+"""
+    User Profile Test Module
+"""
+
+from tests.android.pages.android_whats_new import AndroidWhatsNew
+from tests.android.pages.android_main_dashboard import AndroidMainDashboard
+from tests.android.pages.android_account import AndroidAccunts
+from tests.android.pages.android_profile import AndroidProfile
+from tests.common import strings
+from tests.common.globals import Globals
+
+
+class TestAndroidProfile:
+    """
+    User Profile screen's Test Case
+
+    """
+
+    def test_start_main_dashboard_smoke(self, login, set_capabilities, setup_logging):
+        """
+        Scenarios:
+            Verify Main Dashboard screen is loaded successfully after successful login
+        """
+
+        global_contents = Globals(setup_logging)
+        setup_logging.info('-- Starting {} Test Case'.format(TestAndroidProfile.__name__))
+        if login:
+            setup_logging.info('{} is successfully logged in'.format(global_contents.login_user_name))
+
+        android_whats_new_page = AndroidWhatsNew(set_capabilities, setup_logging)
+        android_whats_new_page.navigate_features()
+        assert android_whats_new_page.navigate_features().text == strings.WHATS_NEW_DONE
+        assert android_whats_new_page.exit_features() == Globals.MAIN_DASHBOARD_ACTIVITY_NAME
+
+    def test_ui_elements_smoke(self, set_capabilities, setup_logging):
+        """
+        Scenarios:
+            Verify that Profile screen will show following contents:
+                Back icon
+                "Profile" as Title
+                Edit
+                Profile Image
+                User Name
+            Verify that Profile screen will show following contents for limited profile:
+                Age limit text
+                Account settings Button
+            Verify that Profile screen will show following contents for limited profile:
+                location
+                Language (if selected)
+                User Bio
+        """
+
+        global_contents = Globals(setup_logging)
+        android_main_dashboard_page = AndroidMainDashboard(set_capabilities, setup_logging)
+        android_accounts_screen = AndroidAccunts(set_capabilities, setup_logging)
+        android_profile_screen = AndroidProfile(set_capabilities, setup_logging)
+
+        assert android_main_dashboard_page.load_account_screen() == global_contents.ACCOUNT_ACTIVITY_NAME
+        assert android_accounts_screen.load_profile_activity() == global_contents.PROFILE_ACTIVITY_NAME
+
+        android_profile_screen.get_navigation_icon().click()
+        assert android_profile_screen.get_account_activity() == global_contents.ACCOUNT_ACTIVITY_NAME
+        assert android_accounts_screen.load_profile_activity() == global_contents.PROFILE_ACTIVITY_NAME
+        android_profile_screen.get_edit_profile_screen().click()
+        android_profile_screen.get_navigation_icon().click()
+        assert android_profile_screen.get_user_profile_image().get_attribute('displayed') == 'true'
+        assert android_profile_screen.get_user_profile_name().get_attribute('displayed') == 'true'
+
+        if android_profile_screen.get_limited_profile_view():
+            assert android_profile_screen.get_profile_age_text_note().text == strings.PROFILE_AGE_LIMIT_TEXT
+            assert android_profile_screen.get_profile_account_settings_button().text \
+                == strings.PROFILE_ACCOUNT_SETTINGS_BUTTON
+        else:
+            assert android_profile_screen.get_user_profile_location().get_attribute('displayed') == 'true'
+
+            if android_profile_screen.get_user_profile_language():
+                assert android_profile_screen.get_user_profile_language().get_attribute('displayed') == 'true'
+
+            assert android_profile_screen.get_user_profile_bio().get_attribute('displayed') == 'true'
