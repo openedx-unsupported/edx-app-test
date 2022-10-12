65645ad9465294bd3e91bec1a73ebd4474239a91
Bilal Awan <bilal.awan@arbisoft.com>
2022-10-12 19:13:21 +0500

    refactor: remove jenkins code from globals and conftest files

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,23 +35,17 @@ def set_capabilities(setup_logging):
     log = setup_logging
     globals_contents = Globals(log)
     desired_capabilities = {}
-    if globals_contents.jenkins is False:
-        log.info('{} - {} - {} - {} - {}'.format(
-            globals_contents.target_environment,
-            globals_contents.login_user_name,
-            globals_contents.login_password,
-            globals_contents.android_platform_version,
-            globals_contents.ios_platform_version
-        ))
-        log.info('- Setting {} capabilities'.format(globals_contents.target_environment))
 
-    if globals_contents.jenkins is True:
-        desired_capabilities['appWaitDuration'] = '50000'
-        desired_capabilities['appPackage'] = globals_contents.AUT_PACKAGE_NAME
-        desired_capabilities['appActivity'] = Globals.SPLASH_ACTIVITY_NAME
-        desired_capabilities['appWaitActivity'] = Globals.NEW_LOGISTRATION_ACTIVITY_NAME
+    log.info('{} - {} - {} - {} - {}'.format(
+        globals_contents.target_environment,
+        globals_contents.login_user_name,
+        globals_contents.login_password,
+        globals_contents.android_platform_version,
+        globals_contents.ios_platform_version
+    ))
+    log.info('- Setting {} capabilities'.format(globals_contents.target_environment))
 
-    elif globals_contents.jenkins is False and globals_contents.target_environment == strings.ANDROID:
+    if globals_contents.target_environment == strings.ANDROID:
         desired_capabilities['platformName'] = strings.ANDROID
         desired_capabilities['platformVersion'] = globals_contents.android_platform_version
         desired_capabilities['deviceName'] = globals_contents.android_device_name
