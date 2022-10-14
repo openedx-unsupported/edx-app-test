1167f399c11fb0d516529ff070f8697fe95f1a0f
Bilal Awan <56023064+bilalawan321@users.noreply.github.com>
2022-10-14 12:23:53 +0500

    refactor: Remove jenkins code (#145)
    * refactor: remove jenkins code from globals and conftest files
    - remove jenkins info from readme file

--- tests/common/globals.py
+++ tests/common/globals.py
@@ -6,7 +6,6 @@ import sys
 import string
 import random
 import enum
-import os
 import yaml
 
 from appium.webdriver.common.mobileby import MobileBy
@@ -72,7 +71,6 @@ class Globals:
         self.fourteenth_existence = 13
         self.fifteenth_existence = 14
         self.sixteenth_existence = 15
-        self.jenkins = True
 
         # Read user_preferences.yml and set globals accordingly
         self.setup_global_environment()
@@ -94,28 +92,21 @@ class Globals:
 
     def setup_global_environment(self):
         """
-        set environment for jenkins and read user_preferences for local run
+        set environment and read user_preferences for local run
         """
 
-        if self.jenkins is False:
-            with open("./tests/user_preferences.yml") as user_file:
-                user_preferences = yaml.safe_load(user_file)
-                user_file.close()
+        with open("./tests/user_preferences.yml") as user_file:
+            user_preferences = yaml.safe_load(user_file)
+            user_file.close()
 
-            self.target_environment = user_preferences.get('Settings').get('target_environment')
-            self.server_url = user_preferences.get('Settings').get('appium_server')
-            self.android_platform_version = user_preferences.get('Settings').get('android_platform_version')
-            self.ios_platform_version = user_preferences.get('Settings').get('ios_platform_version')
-            self.aut_current_path = user_preferences.get('Settings').get('aut_current_path')
-            self.aut_latest_path = user_preferences.get('Settings').get('aut_latest_path')
-            self.login_user_name = user_preferences.get('User').get('login_user_name')
-            self.login_password = user_preferences.get('User').get('login_password')
-
-        else:
-            self.server_url = 'http://127.0.0.1:4723/wd/hub'
-            self.target_environment = 'Android'
-            self.login_user_name = os.environ['USER_NAME']
-            self.login_password = os.environ['USER_PASSWORD']
+        self.target_environment = user_preferences.get('Settings').get('target_environment')
+        self.server_url = user_preferences.get('Settings').get('appium_server')
+        self.android_platform_version = user_preferences.get('Settings').get('android_platform_version')
+        self.ios_platform_version = user_preferences.get('Settings').get('ios_platform_version')
+        self.aut_current_path = user_preferences.get('Settings').get('aut_current_path')
+        self.aut_latest_path = user_preferences.get('Settings').get('aut_latest_path')
+        self.login_user_name = user_preferences.get('User').get('login_user_name')
+        self.login_password = user_preferences.get('User').get('login_password')
 
     def wait_and_get_element(self, driver, element_locator, optional_time=None):    # pylint: disable=inconsistent-return-statements
         """
